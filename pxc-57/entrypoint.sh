02cf7d59fa013fe444e8dd26819983f562671301
Vladimir Petrukhin <man4j@ya.ru>
2017-02-28 19:38:41 +0300

    Update entrypoint.sh

--- pxc-57/entrypoint.sh
+++ pxc-57/entrypoint.sh
@@ -6,6 +6,10 @@ if [ "${1:0:1}" = '-' ]; then
 	CMDARG="$@"
 fi
 
+#get server_id from ip address
+ipaddr=$(hostname -i | awk ' { print $1 } ')
+server_id=$(echo $ipaddr | tr . '\n' | awk '{s = s + $1} END{print s}')
+
 if [ -z "$CLUSTER_NAME" ]; then
 	echo >&2 'Error:  You need to specify CLUSTER_NAME'
 	exit 1
@@ -103,56 +107,16 @@ fi
 	touch $DATADIR/init.ok
 	chown -R mysql:mysql "$DATADIR"
 
-if [ -z "$DISCOVERY_SERVICE" ]; then
-	cluster_join=$CLUSTER_JOIN
-else
+#My Patch=============================================================================
 
-echo
-echo 'Registering in the discovery service'
-echo
+cluster_join=$CLUSTER_JOIN
 
-function join {
-  local IFS="$1"
-  shift
-  joined=$(tr "$IFS" '\n' <<< "$*" | sort -un | tr '\n' "$IFS")
-  echo "${joined%?}"
-}
+/etc/init.d/xinetd start
 
-# Read the list of registered IP addresses
-set +e
+echo "Starting listener for slave requests"
+/usr/bin/master_nc >> /tmp/master_nc.log &
 
-ipaddr=$(hostname -i | awk ' { print $1 } ')
-hostname=$(hostname)
-
-curl http://$DISCOVERY_SERVICE/v2/keys/pxc-cluster/queue/$CLUSTER_NAME -XPOST -d value=$ipaddr -d ttl=60
-
-#get list of IP from queue 
-i=$(curl http://$DISCOVERY_SERVICE/v2/keys/pxc-cluster/queue/$CLUSTER_NAME | jq -r '.node.nodes[].value')
-
-# this remove my ip from the list
-i1="${i[@]/$ipaddr}"
-cluster_join1=$(join , $i1)
-
-# Register the current IP in the discovery service
-
-# key set to expire in 30 sec. There is a cronjob that should update them regularly
-curl http://$DISCOVERY_SERVICE/v2/keys/pxc-cluster/$CLUSTER_NAME/$ipaddr/ipaddr -XPUT -d value="$ipaddr" -d ttl=30
-curl http://$DISCOVERY_SERVICE/v2/keys/pxc-cluster/$CLUSTER_NAME/$ipaddr/hostname -XPUT -d value="$hostname" -d ttl=30
-curl http://$DISCOVERY_SERVICE/v2/keys/pxc-cluster/$CLUSTER_NAME/$ipaddr -XPUT -d ttl=30 -d dir=true -d prevExist=true
-
-#i=`curl http://$DISCOVERY_SERVICE/v2/keys/pxc-cluster/$CLUSTER_NAME/ | jq -r '.node.nodes[].value'`
-i=$(curl http://$DISCOVERY_SERVICE/v2/keys/pxc-cluster/$CLUSTER_NAME/?quorum=true | jq -r '.node.nodes[]?.key' | awk -F'/' '{print $(NF)}')
-# this remove my ip from the list
-i2="${i[@]/$ipaddr}"
-cluster_join2=$(join , $i1)
-cluster_join=$(join , $i1 $i2 )
-echo "Joining cluster $cluster_join"
-
-
-/usr/bin/clustercheckcron monitor monitor 1 /var/lib/mysql/clustercheck.log 1 & 
-set -e
-
-fi
+#======================================================================================
 
 #--log-error=${DATADIR}error.log
 exec mysqld --user=mysql --wsrep_cluster_name=$CLUSTER_NAME --wsrep_cluster_address="gcomm://$cluster_join" --wsrep_sst_method=xtrabackup-v2 --wsrep_sst_auth="xtrabackup:$XTRABACKUP_PASSWORD" --wsrep_node_address="$ipaddr" $CMDARG
