f9ca1cd040daf70be3120f641a34a9808e011be0
Vladimir Petrukhin <man4j@ya.ru>
2017-03-01 11:21:22 +0300

    Update ps-entry.sh

--- percona-server-master-slave/ps-entry.sh
+++ percona-server-master-slave/ps-entry.sh
@@ -3,7 +3,7 @@ set -e
 
 # if command starts with an option, prepend mysqld
 if [ "${1:0:1}" = '-' ]; then
-	CMDARG="$@"
+  CMDARG="$@"
 fi
 
 #get server_id from ip address
@@ -28,14 +28,14 @@ cd $DATADIR
 echo "Receiving stream ..."
 
 nc -l -p 4565 | xbstream -x
-innobackupex --apply-log --use-memory=2G ./
+innobackupex --apply-log --use-memory=1G ./
 slavepass="$(pwmake 128)"
 mysql -h${MASTER_HOST} -uroot -p${MYSQL_ROOT_PASSWORD} -e "GRANT REPLICATION SLAVE ON *.*  TO 'repl'@'$ipaddr' IDENTIFIED BY '$slavepass';"
 chown -R mysql:mysql "$DATADIR"
 
 # start slave 
 echo "Starting slave..."
-mysqld --user=mysql --server-id=$server_id  $CMDARG &
+mysqld --user=mysql --server-id=$server_id --gtid-mode=ON --enforce-gtid-consistency --log-bin=/var/log/mysql/mysqlbinlog $CMDARG &
 pid="$!"
   
 echo "Started with PID $pid, waiting for initialization..."
@@ -56,4 +56,4 @@ if [ "$i" = 0 ]; then
 fi
 
 echo "Slave initialized, connecting to master..."
-mysql -uroot -p${MYSQL_ROOT_PASSWORD} -e "CHANGE MASTER TO MASTER_HOST='${MASTER_HOST}', MASTER_USER='repl', MASTER_PASSWORD='$slavepass', MASTER_AUTO_POSITION = 1; START SLAVE"
+mysql -uroot -p${MYSQL_ROOT_PASSWORD} -e "CHANGE MASTER TO MASTER_HOST='${MASTER_HOST}', MASTER_USER='repl', MASTER_PASSWORD='$slavepass', MASTER_AUTO_POSITION = 1; START SLAVE;"
